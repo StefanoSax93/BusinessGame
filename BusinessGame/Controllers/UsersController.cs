167ce2c7dbd66c1d0b0e14b191a29cc2ea017e76
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2023-06-12 11:34:38 +0200

    Add project files.

---
+++ BusinessGame/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessGame.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+
+namespace BusinessGame.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly BusinessGameContext _context;
+
+        public UsersController(BusinessGameContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ElencoUtenti()
+        {
+            var query = from u in _context.Users
+                        select u;
+
+            return View(query);
+        }
+
+        public IActionResult NuovoUtente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NuovoUtente(Users utente)
+        {
+            _context.Users.Add(utente);
+            _context.SaveChanges();
+            return RedirectToAction("ElencoUtenti");
+        }
+
+        public IActionResult ModificaUtente(int? id)
+        {
+            var utente = _context.Users.Find(id);
+            return View(utente);
+        }
+
+        [HttpPost]
+        public IActionResult ModificaUtente(Users utente)
+        {
+            _context.Users.Update(utente);
+            _context.SaveChanges();
+            return RedirectToAction("ElencoUtenti");
+        }
+    }
+}
---
+++ BusinessGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusinessGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
